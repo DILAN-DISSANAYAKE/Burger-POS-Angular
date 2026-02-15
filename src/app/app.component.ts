645680af2c732caded2c6f7efe0ea2e155a1504f
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2026-02-16 02:19:43 +0530

    Added AOS for better animations and setup it to loads in browser to avoid SSR errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,afterNextRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Burger-POS-Angular';
+  constructor() {
+
+    afterNextRender(() => {
+      AOS.init({
+        duration: 800, 
+        once: true,   
+        offset: 100    
+      });
+    });
+  }
 }
