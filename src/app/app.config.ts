1243f1b2b20646f8651c29f75659a226d7c47d78
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2026-02-16 21:04:25 +0530

    build: setup ngx-toastr, provideAnimations, and enable smooth scroll restoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'enabled'
+
+
+    })),
+    provideAnimations(), 
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      timeOut: 1500,                      
+      progressBar: true,                  
+      // preventDuplicates: true,            
+    }),
+  ]
 };
