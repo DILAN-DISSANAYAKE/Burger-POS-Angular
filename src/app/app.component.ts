63b87b794a0242f9000c824fc04e98c5ccd6a712
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2026-02-16 21:02:39 +0530

    Added default AOS animation settings for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component,afterNextRender } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import AOS from 'aos';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -10,14 +8,5 @@ import AOS from 'aos';
 })
 export class AppComponent {
   title = 'Burger-POS-Angular';
-  constructor() {
-
-    afterNextRender(() => {
-      AOS.init({
-        duration: 800, 
-        once: true,   
-        offset: 100    
-      });
-    });
-  }
+  
 }
