8193dd673c4ebe6c2fe9658b12cbcfd34e461608
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2026-02-16 08:15:14 +0530

    Created Main for UI management and added aos settings

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,25 @@
+import { Component,afterNextRender } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import AOS from 'aos';
+import { FooterComponent } from "../footer/footer.component";
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-main',
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  constructor() {
+
+    afterNextRender(() => {
+      AOS.init({
+        duration: 800, 
+        once: true,   
+        offset: 100    
+      });
+    });
+  }
+}
