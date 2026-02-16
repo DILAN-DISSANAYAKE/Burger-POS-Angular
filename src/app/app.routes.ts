9f073cf1b5dbd2f129e92dfe3f7eea1509e448c5
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2026-02-16 21:06:00 +0530

    update router config to use children routes for organized navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'login',
+    component: LoginComponent 
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'menu/:categoryName', component: MenuComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+      ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
